500194bb5542f5f6afe5e01becd473d6f774cd97
Viktoria Koncserova <101568551+viktoriakoncserova@users.noreply.github.com>
2025-05-19 09:50:34 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 from main import AnomalyDetectionPipeline
+import os
 
 if __name__ == "__main__":
-    email_receivers = ["xkoncserova@stuba.sk"]
+    email_receivers = [os.getenv("RECEIVER")]
     timeFrom_str = "2025-04-15 14:00:00"
     timeTo_str = "2025-04-15 14:30:00"
 
